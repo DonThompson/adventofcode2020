0fdcd200d799c79a2a9b62c145b7b9e4678a1f33
Don Thompson <thompsondn@gmail.com>
2020-12-13 19:46:07 -0500

    day3++

---
+++ day01/Day03.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace adventofcode2020
+{
+    public class Day03
+    {
+        public static void DoDay03()
+        {
+            bool[,] map = MapInput();
+            Console.WriteLine($"Day 3a:  {CountTreesOnSlope(map, 3, 1)}");
+
+            //b
+            uint val1 = CountTreesOnSlope(map, 1, 1);
+            uint val2 = CountTreesOnSlope(map, 3, 1);
+            uint val3 = CountTreesOnSlope(map, 5, 1);
+            uint val4 = CountTreesOnSlope(map, 7, 1);
+            uint val5 = CountTreesOnSlope(map, 1, 2);
+            uint result = val1 * val2 * val3 * val4 * val5;
+            Console.WriteLine($"Day 3b:  {result}");
+        }
+
+        private static uint CountTreesOnSlope(bool[,] map, int incX, int incY)
+        {
+            uint treesEncountered = 0;
+            //Starting position
+            int x = 0, y = 0;
+            //Inc first move
+            x += incX;
+            y += incY;
+
+            int mapHeight = map.GetLength(0);
+            int mapWidth = map.GetLength(1);
+            while(y < mapHeight)
+            {
+                //Is it off the right edge?  if so we need to wrap
+                if (x >= mapWidth)
+                    x -= mapWidth;
+
+                //Is it a tree?
+                if (map[y, x])
+                    treesEncountered++;
+
+                //Move to next position in the slope
+                x += incX;
+                y += incY;
+            }
+
+            return treesEncountered;
+        }
+
+        private static bool[,] MapInput()
+        {
+            //Hardcoded based on input
+            bool[,] map = new bool[323, 31];
+
+            int i = 0;
+            foreach(string line in File.ReadAllLines("input03.txt"))
+            {
+                int j = 0;
+                foreach(char c in line)
+                {
+                    if (c == '.')
+                        map[i,j] = false;
+                    else
+                        map[i,j] = true;
+                    j++;
+                }
+                i++;
+            }
+
+            return map;
+        }
+    }
+}
--- day01/Program.cs
+++ day01/Program.cs
@@ -1,16 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-
-namespace adventofcode2020
+﻿namespace adventofcode2020
 {
     class Program
     {
         static void Main(string[] args)
         {
             //Day01.DoDay01();
-            Day02.DoDay02();
+            //Day02.DoDay02();
+            Day03.DoDay03();
 
         }
     }
