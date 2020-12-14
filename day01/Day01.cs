7f611d553fa769e6c75a710044d75f5eea146a98
Don Thompson <thompsondn@gmail.com>
2020-12-13 19:00:35 -0500

    quick refactor to day01 class

---
+++ day01/Day01.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace day01
+{
+    public static class Day01
+    {
+        public static void DoDay01()
+        {
+            Console.WriteLine($"Day 1a:  {FindResultOf2()}");
+            Console.WriteLine($"Day 1b:  {FindResultOf3()}");
+        }
+
+        private static int FindResultOf2()
+        {
+            const int target = 2020;
+
+            List<string> input = File.ReadAllLines("input1.txt").ToList();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                int first = Int32.Parse(input[i]);
+
+                //Only need to test from our neighbor up, the below already tested us
+                for (int j = i + 1; j < input.Count; j++)
+                {
+                    int second = Int32.Parse(input[j]);
+
+                    if (first + second == target)
+                    {
+                        return first * second;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        private static int FindResultOf3()
+        {
+            const int target = 2020;
+
+            List<string> input = File.ReadAllLines("input1.txt").ToList();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                int first = Int32.Parse(input[i]);
+
+                //Only need to test from our neighbor up, the below already tested us
+                for (int j = i + 1; j < input.Count; j++)
+                {
+                    int second = Int32.Parse(input[j]);
+
+                    for (int k = j + 1; k < input.Count; k++)
+                    {
+                        int third = Int32.Parse(input[k]);
+
+                        if (first + second + third == target)
+                        {
+                            return first * second * third;
+                        }
+
+                    }
+
+                }
+            }
+
+            return -1;
+        }
+    }
+}
--- day01/Program.cs
+++ day01/Program.cs
@@ -7,67 +7,10 @@ namespace day01
 {
     class Program
     {
-        private static int FindResultOf2()
-        {
-            const int target = 2020;
-
-            List<string> input = File.ReadAllLines("input1.txt").ToList();
-
-            for (int i = 0; i < input.Count; i++)
-            {
-                int first = Int32.Parse(input[i]);
-
-                //Only need to test from our neighbor up, the below already tested us
-                for (int j = i + 1; j < input.Count; j++)
-                {
-                    int second = Int32.Parse(input[j]);
-
-                    if (first + second == target)
-                    {
-                        return first * second;
-                    }
-                }
-            }
-
-            return -1;
-        }
-
-        private static int FindResultOf3()
-        {
-            const int target = 2020;
-
-            List<string> input = File.ReadAllLines("input1.txt").ToList();
-
-            for (int i = 0; i < input.Count; i++)
-            {
-                int first = Int32.Parse(input[i]);
-
-                //Only need to test from our neighbor up, the below already tested us
-                for (int j = i + 1; j < input.Count; j++)
-                {
-                    int second = Int32.Parse(input[j]);
-
-                    for(int k = j + 1; k < input.Count; k++)
-                    {
-                        int third = Int32.Parse(input[k]);
-
-                        if (first + second + third == target)
-                        {
-                            return first * second * third;
-                        }
-
-                    }
-
-                }
-            }
-
-            return -1;
-        }
-
         static void Main(string[] args)
         {
-            Console.WriteLine($"Day 1a:  {FindResultOf2()}");
-            Console.WriteLine($"Day 1b:  {FindResultOf3()}");
+            //Day01.DoDay01();
+
         }
     }
 }
